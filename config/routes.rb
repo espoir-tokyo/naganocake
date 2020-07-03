aee3d4a5122d7168f7da71346fa1f0679d3a8d35
Yusuke Ooba <yusukeooba@YusukenoMacBook-Pro.local>
2020-07-03 16:25:00 +0900

    管理者のログインページとトップページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, controllers: {
+  sessions:      'admins/sessions',
+  passwords:     'admins/passwords',
+  registrations: 'admins/registrations'
+}
   devise_for :customers
+  namespace :admins do
+  	get '/' => 'homes#top'
+  end
+  #get 'admins' => 'admins/homes#top'
+  #resources :homes, only: [:top]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
