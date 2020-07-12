c4ea9d39d4340e606926c350b2cfd5ba09d601e1
mona-creator <astronaut1129@gmail.com>
2020-07-12 13:53:22 +0900

    注文機能追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
 	attachment :image
 	belongs_to :genre
 	has_many :cart_items
+	has_many :order_items
 	validates :name, presence: true
 	validates :description, presence: true
 	validates :genre_id, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_09_034321) do
+ActiveRecord::Schema.define(version: 2020_07_11_103507) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -67,6 +67,16 @@ ActiveRecord::Schema.define(version: 2020_07_09_034321) do
     t.integer "genre_id"
     t.integer "price"
     t.boolean "sale_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "order_id"
+    t.integer "quantity"
+    t.integer "perchase_price"
+    t.integer "item_status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
