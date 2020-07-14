cf3308e9d6e59e600aedc7174ee232037c3db93c
愿山聖 <yoyo@sunaoyamaseinoMacBook-Pro.local>
2020-07-14 14:47:28 +0900

    [add]top_contorollerの記述

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,6 @@
 class HomesController < ApplicationController
 	def top
-		@genres = Genre.all
+		@genres = Genre.where(flag: 'true')
 		@random = Item.order("RANDOM()").where(sale_status: 'true').limit(4)
 	end
 
